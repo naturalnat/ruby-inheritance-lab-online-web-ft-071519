b2e166d12da57642107a971e236eb5e4ad21b71a
Natalie Taktachev <ntaktach@gmail.com>
2019-08-04 07:30:54 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,11 +1,4 @@
 class User
   attr_accessor :first_name, :last_name
 
-  @@all = []
-
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
-
 end
